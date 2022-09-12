fea8937714aed738814531451a1bedc745466266
davidberth <odlantern@gmail.com>
2022-09-11 23:32:54 -0400

    Implemented twisting and discs with line segments offsets to prevent intersection. Next, I will build up a list of the line segments and discs for generation of the lights and buildings.

--- alg_city/branching.py
+++ alg_city/branching.py
@@ -3,58 +3,90 @@ import os
 import colorsys
 import numpy as np
 
-import geopandas as gpd
+# TODO remove line segments from discs
+# TODO add local twisting
+# TODO create a list of line segments
+# TODO create a list of discs
+# TODO create street lights along line segments
+# TODO create street lights in discs
 
+# parameters
+inner_disc_radius = 0.13
+twist_angle = 0.19
 
 out_file = r'output\test1.png'
+pi2 = np.pi * 2.0
 
 size = (2048, 2048)
-size_half = (size[0]//2, size[1]//2)
-origin = (0.0 , 0.0)
+size_half = (size[0] // 2, size[1] // 2)
+origin = (0.0, 0.0)
+zoom = 0.25
+
+line_list = []
+disc_list = []
 
 # create the canvas
 surface = cairo.ImageSurface(cairo.FORMAT_RGB24, *size)
 ctx = cairo.Context(surface)
-ctx.scale(*size_half)
-ctx.translate(1.0, 1.0)
+ctx.scale(size_half[0] * zoom, size_half[1] * zoom)
+ctx.translate(1.0 / zoom, 1.0 / zoom)
+
 
 def rand(low, high):
     return np.random.rand() * (high - low) + low
 
 
-def branch(start, angle, angle_off, length, length_off, width, hue, saturation, value,
-           level=0):
+def branch(start, angle, angle_off, length, length_off, start_offset,  width,
+           hue, saturation, value, twist, level=0):
     if level > rand(4.9, 9.1):
         return
     ctx.set_line_width(width)
     rgb = colorsys.hsv_to_rgb(hue, saturation, value)
     ctx.set_source_rgb(*rgb)
-    ctx.move_to(*start)
-    local_length = length + rand(-length_off, length_off)
-    end = start[0] + np.cos(angle) * local_length, start[1] + np.sin(angle) * local_length
-    ctx.line_to(*end)
+    line_start = start[0] + np.cos(angle) * start_offset, \
+        start[1] + np.sin(angle) * start_offset
+
+    local_length = length + rand(0, length_off)
+    end = start[0] + np.cos(angle) * local_length, \
+          start[1] + np.sin(angle) * local_length
+
+    num_branches = np.random.choice([1, 3, 5], p=[0.7, 0.2, 0.1])
+
+    if num_branches > 3:
+        end_radius = np.sqrt(end[0]**2 + end[1]**2)
+        end_offset = 0.05/(end_radius**0.3)
+        ctx.arc(*end, end_offset, 0.0, pi2)
+    else:
+        end_offset = 0.0
+
+    ctx.move_to(*line_start)
+
+
+    line_end = start[0] + np.cos(angle) * (local_length - end_offset), \
+        start[1] + np.sin(angle) * (local_length - end_offset)
+    ctx.line_to(*line_end)
     ctx.stroke()
 
-    num_branches = np.random.choice([2,3], p=[0.75, 0.25])
-    if num_branches == 2:
-        ang_inc = 0.4
-    else:
-        ang_inc = 0.2
+    ang_inc = 0.25
+
+    ang_start = ((num_branches - 1.0)/2.0) * ang_inc
 
 
-    for angle_loc in np.arange(-0.2, 0.21, ang_inc):
+    for angle_loc in np.arange(-ang_start, ang_start + 0.1, ang_inc):
         angle_ran = rand(-angle_off, angle_off)
-        branch(end, angle+angle_loc + angle_ran, 0.15, local_length*.85,
-               local_length*.38, width*.8, hue*.8,
-           saturation, value, level+1)
+        branch(end, angle + angle_loc + angle_ran + twist, 0.15, local_length * .85,
+               local_length * .38, end_offset, width * .8, hue * .8,
+               saturation, value, twist, level + 1)
 
-start_angle = rand(0.0, 0.5)
-for ang in np.arange(0, np.pi * 2.0, 0.6):
+
+start_angle = rand(0.0, 0.3)
+ctx.arc(*origin, inner_disc_radius, 0.0, pi2)
+for ang in np.arange(start_angle, pi2, 0.5):
     hue = rand(0.0, 1.0)
-    length = rand(0.10, 0.14)
+    length = rand(0.20, 0.24)
+    twist_factor = np.random.choice([-1.0, 1.0])
     branch(origin, ang + rand(-0.1, 0.1),
-           ang/8.0, length, length * 0.38, 0.01, hue, 0.3, 0.8)
+           ang / 8.0, length, length * 0.38, inner_disc_radius, 0.01,
+           hue, 0.3, 0.8, twist_angle * twist_factor)
 
 surface.write_to_png(out_file)
-
-
