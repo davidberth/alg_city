6ebf281beef4c74799e067d5719b728f7837597b
David Berthiaume <david.berthiaume@avalara.com>
2022-09-15 00:14:51 -0400

    Implement street light objects along side of paths.

--- alg_city/branching.py
+++ alg_city/branching.py
@@ -3,23 +3,27 @@ import os
 import colorsys
 import numpy as np
 
+np.random.seed(44444)
+
 
 
 # TODO create street lights along line segments
 # TODO create street lights in discs
 
 # parameters
+size = (2048, 2048)
 inner_disc_radius = 0.13
 twist_angle = 0.19
 disc_mult = 0.07
+zoom = 1.0
+
 
 out_file = r'output\test1.png'
 pi2 = np.pi * 2.0
 
-size = (2048, 2048)
 size_half = (size[0] // 2, size[1] // 2)
 origin = (0.0, 0.0)
-zoom = 0.25
+
 
 line_list = []
 disc_list = []
@@ -37,7 +41,7 @@ def rand(low, high):
 
 def branch(start, angle, angle_off, length, length_off, start_offset,  width,
            hue, saturation, value, twist, level=0):
-    if level > rand(4.9, 9.1):
+    if level > rand(1.9, 3.1):
         return
     ctx.set_line_width(width)
     rgb = colorsys.hsv_to_rgb(hue, saturation, value)
@@ -77,7 +81,7 @@ def branch(start, angle, angle_off, length, length_off, start_offset,  width,
     for angle_loc in np.arange(-ang_start, ang_start + 0.1, ang_inc):
         angle_ran = rand(-angle_off, angle_off)
         branch(end, angle + angle_loc + angle_ran + twist, 0.15, local_length * .85,
-               local_length * .38, end_offset, width * .8, hue * .8,
+               local_length * .38, end_offset, width * .6, hue * .8,
                saturation, value, twist, level + 1)
 
 
@@ -97,13 +101,27 @@ for line in line_list:
     start, end, level = line
     start = np.array(start)
     end = np.array(end)
-    for l in np.arange(0, 1, 0.1):
-        coord = start * (1-l) + end * l
-        print (*coord)
-        ctx.set_source_rgb(1,1,1)
-        ctx.set_line_width(0.2)
-        ctx.arc(*coord, 0.4, 0.2, pi2)
+    diff = end - start
+    perp_dist_factor = 0.014 - (level * 0.0018)
 
+    perpen = (-diff[1], diff[0]) / np.linalg.norm(diff)
+
+    ctx.set_line_width(0.003)
+    inc = 0.035 * (level + 1.2)
+    inc = max(0.13, inc)
+    for l in np.arange(0, 1, inc):
+        coord = start * (1.0-l) + end * l
+        coordl = coord - perpen * perp_dist_factor
+        coordr = coord + perpen * perp_dist_factor
+
+        ctx.set_source_rgb(float(level) * .25, 0.1, 0.0)
+
+
+        ctx.arc(*coordl, 0.003, 0.0, pi2)
+        ctx.stroke()
+
+        ctx.arc(*coordr, 0.003, 0.0, pi2)
+        ctx.stroke()
 
 
 
